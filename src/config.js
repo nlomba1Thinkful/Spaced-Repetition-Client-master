1b13c6af5f0b29727b3400589ef6aec1c77ca191
nick <nlomba1@gmail.com>
2021-03-03 13:32:50 -0500

    API_Endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT: 'https://obscure-island-06040.herokuapp.com/api',
-  //API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
