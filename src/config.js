6e5ba9cf7a8c2355f2e1809bb0f2796d09c923e3
nick <nlomba1@gmail.com>
2021-03-03 22:16:30 -0500

    new endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://rcs-spaced-repetition.herokuapp.com/api',
+  API_ENDPOINT: 'https://obscure-island-06040.com/api',
   // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
