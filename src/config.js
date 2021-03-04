f8942028a38867d804ff9c413a8fbbb990680054
nick <nlomba1@gmail.com>
2021-03-04 12:19:01 -0500

    obscure

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://thawing-plains-80929.herokuapp.com/api',
+  API_ENDPOINT: 'https://obscure-island-06040.com/api',
   //API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
